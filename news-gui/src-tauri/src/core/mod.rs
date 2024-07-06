b15f31dbc2c73527b0c2e09037fb00ffd870d332
Vixu <lonevixu@gmail.com>
2024-07-06 16:18:36 +0200

    use r2d2::Pool

--- news-gui/src-tauri/src/core/mod.rs
+++ news-gui/src-tauri/src/core/mod.rs
@@ -1,28 +1,27 @@
 use std::{
     fs,
-    sync::{Mutex, MutexGuard, OnceLock},
+    sync::{Mutex, OnceLock},
 };
 
-use rusqlite::Connection;
 use shared::{
-    db::init_db,
+    db::{self, DbPool},
     rss_feeds::{FeedUrl, RssItem},
 };
 use tauri::AppHandle;
 
 pub struct ApplicationState {
-    pub db: OnceLock<Mutex<Connection>>,
+    pub db: OnceLock<DbPool>,
     pub feeds: Mutex<Vec<FeedUrl>>,
     pub items: Mutex<Vec<RssItem>>,
 }
 
 impl ApplicationState {
-    pub fn db(&self) -> MutexGuard<'_, Connection> {
-        self.db.get().unwrap().lock().unwrap()
+    pub fn db(&self) -> &DbPool {
+        self.db.get().unwrap()
     }
 }
 
-pub fn initialize_database(app_handle: &AppHandle) -> Result<Connection, rusqlite::Error> {
+pub fn initialize_database(app_handle: &AppHandle) -> db::Result<DbPool> {
     let app_dir = app_handle
         .path_resolver()
         .app_data_dir()
@@ -30,7 +29,7 @@ pub fn initialize_database(app_handle: &AppHandle) -> Result<Connection, rusqlit
     println!("App dir: {:?}", &app_dir);
     fs::create_dir_all(&app_dir).expect("Failed to create app data directory");
     let sqlite_path = app_dir.join("news.db");
-    let db = init_db(&sqlite_path)?;
+    let db = db::init(&sqlite_path)?;
 
     Ok(db)
 }
